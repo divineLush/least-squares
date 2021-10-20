1e30aa3150053a465f3f351192f5d75dad51d2ee
divineLush <homeoflucifer@gmail.com>
2021-10-20 06:39:49 -0400

    refactor

--- geninput.py
+++ geninput.py
@@ -2,13 +2,8 @@ import csv
 from secrets import randbelow
 
 
-rows = []
 pointsnum = randbelow(150) + 10
-
-for i in range(pointsnum):
-    point = (randbelow(100), randbelow(100))
-    rows.append(point)
-
+rows = [(randbelow(100), randbelow(100)) for i in range(pointsnum)]
 
 with open('input.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
